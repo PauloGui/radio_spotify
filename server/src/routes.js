95f6abba02968de89ab8964bba9160af9802c79d
Paulo Sousa <paulopgss@gmail.com>
2022-03-17 15:13:53 -0300

    project

---
+++ server/src/routes.js
@@ -0,0 +1,35 @@
+import config from "./config.js";
+import { Controller } from "./controller.js";
+import { logger } from "./util.js";
+
+const {
+  location,
+  pages: { homeHTML },
+} = config;
+
+const controller = new Controller();
+
+async function routes(request, response) {
+  const { method, url } = request;
+
+  if (method === "GET" && url === "/") {
+    response.writeHead(302, {
+      Location: location.home,
+    });
+
+    response.end();
+  }
+
+  if (method === "GET" && url === "/home") {
+    const { stream } = await controller.getFileStream(homeHTML);
+
+    return stream.pipe(response);
+  }
+  return response.end("Hello World!");
+}
+
+export function handler(request, response) {
+  return routes(request, response).catch((error) => {
+    logger.error(`Error: ${error.stack}`);
+  });
+}
